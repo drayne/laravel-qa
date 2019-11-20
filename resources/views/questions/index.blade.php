f71550c0915134dbf6056c4d962edcad5f63fb08
vedran <bojicic@gmail.com>
2019-11-20 21:03:24 +0100

    creating question show page and model accessor

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -31,7 +31,7 @@
                                 </div>
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
-                                        <h3 class="m-0"><a href="{{ route('questions.show', $question->id)  }}">{{$question->title}}</a></h3>
+                                        <h3 class="m-0"><a href="{{ route('questions.show', $question->slug)  }}">{{$question->title}}</a></h3>
                                         <div class="ml-auto">
                                             <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                             <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
