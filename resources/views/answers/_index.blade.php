5276bfa809650b937683189e11c52eea94c52b66
vedran <bojicic@gmail.com>
2020-01-05 01:15:29 +0100

    cleaning views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -9,45 +9,7 @@
                     <hr>
                     @include('layouts._messages')
                     @foreach($answers as $answer)
-                        <div class="media">
-                            <div class="d-flex flex-column vote-controls">
-                                @include('shared._vote', [
-                                    'model' => $answer
-                                ])
-                                @include('shared._accept', [
-                                    'model' => $answer
-                                ])
-                            </div>
-                            <div class="media-body">
-                                {!! $answer->body_html !!}
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            @can('update', $answer)
-                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-
-                                            @can('delete', $answer)
-                                                <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure')">Delete</button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </div>
-                                    <div class="col-4">
-                                    </div>
-                                    <div class="col-4">
-                                        @include('shared._author', [
-                                            'model' => $answer,
-                                            'label' => 'answered'
-                                            ])
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
+                        @include('answers._answer')
                     @endforeach
                 </div>
             </div>
